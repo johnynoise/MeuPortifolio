bedff4d9de0aefbfab1f56408002b0d7bb0731d3
Johny Noise <79932410+johnynoise@users.noreply.github.com>
2025-08-04 22:40:41 -0400

    Atualização de portifolio

--- script.js
+++ script.js
@@ -2,53 +2,57 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
-     $('.scroll-up-btn').click(function(){
-         $('html').animate({scrollTop: 0});
-     });
 
-     var typed = new Typed(".typing", {
-         strings:["Desenvolvedor", "Apaixonado por Tecnologia", "Bílingue"],
-         typeSpeed:100,
-         backSpeed:60,
-         loop:true
-     });
-     var typed = new Typed(".typing-2", {
-        strings:["Desenvolvedor", "Apaixonado por Tecnologia", "Bílingue"],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true
+    $('.scroll-up-btn').click(function(){
+        $('html, body').animate({scrollTop: 0}, 500);
+    });
+
+    var typed = new Typed(".typing", {
+        strings:["Desenvolvedor", "Devops", "Engenheiro de Software", "Analista de Sistemas"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed2 = new Typed(".typing-2", {
+        strings:["Desenvolvedor", "Devops", "Engenheiro de Software", "Analista de Sistemas"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
+        $('.menu-btn ion-icon').toggleClass("active");
     });
+
     $('.carousel').owlCarousel({
-        margin:20,
-        loop:true,
-        autoplayTimeOut:2000,
-        autoplayHoverPauser:true,
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
         responsive:{
             0:{
-                items:1,
-                nav:false
+                items: 1,
+                nav: false
             },
             600:{
-                items:2,
-                nav:false
+                items: 2,
+                nav: false
             },
             1000:{
-                items:3,
-                nav:false
+                items: 3,
+                nav: false
             }
         }
     });
